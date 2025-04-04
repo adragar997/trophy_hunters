7495b14eba9c024e7c2aeb28187ee13de199620c
Alexander <adragar997@fp.iesromerovargas.com>
2025-04-04 12:57:24 +0200

    Use os to extract url from .env file

--- trophy_hunters/views.py
+++ trophy_hunters/views.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
 import requests
+import os
 
 class GameList(APIView):
     def get(self, request):
-        url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+        url = f'{os.environ['URL_GAME_LIST']}'
         games = []
         data = requests.get(url).json()
 
