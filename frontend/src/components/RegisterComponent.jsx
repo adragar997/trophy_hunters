04114f830ac5f5ea76367bffa1b36b3ac3496952
Alexander <adragar997@fp.iesromerovargas.com>
2025-05-04 15:58:32 +0200

    Change form and use new formdata to send the register form

--- frontend/src/components/RegisterComponent.jsx
+++ frontend/src/components/RegisterComponent.jsx
@@ -15,9 +15,26 @@ function RegisterComponent() {
         }
     )
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData)
+        const payload = new FormData();
+
+        payload.append("username",  formData.username);
+        payload.append("password",  formData.password);
+        payload.append("avatar",  formData.avatar);
+        payload.append("banner",  formData.banner);
+        payload.append("bio",       formData.bio);
+        payload.append("firstname", formData.firstname);
+        payload.append("lastname",  formData.lastname);
+        payload.append("birthdate", formData.birthdate);
+
+        const response = await fetch('http://127.0.0.1:8000/trophyhunters/register/',{
+            method: "POST",
+            body: payload,
+        })
+
+        const data = await response.json()
+        console.log(data)
     }
 
     const handleChange = (e) => {
@@ -35,28 +52,28 @@ function RegisterComponent() {
             <h1>Formulario de registro</h1>
             <form method="post" onSubmit={handleSubmit}>
                 <label htmlFor="username">Usuario</label>
-                <input type="text" id="username" onChange={handleChange}/>
+                <input type="text" id="username" name="username" onChange={handleChange}/>
 
-                <label htmlFor="password">Foto de avatar</label>
-                <input type="password" id="password" onChange={handleChange}/>
+                <label htmlFor="password">Contraseña</label>
+                <input type="password" id="password" name="password" onChange={handleChange}/>
 
                 <label htmlFor="avatar">Foto de avatar</label>
-                <input type="file" accept="image/*" id="avatar" onChange={handleImage}/>
+                <input type="file" id="avatar" name="avatar" onChange={handleImage}/>
 
                 <label htmlFor="banner">Banner</label>
-                <input type="file" accept="image/*" id="banner" onChange={handleImage}/>
+                <input type="file" id="banner" name="banner" onChange={handleImage}/>
 
                 <label htmlFor="bio">Sobre mi</label>
-                <textarea id="bio" cols="30" rows="10" onChange={handleChange}></textarea>
+                <textarea id="bio" cols="30" rows="10" name="bio" onChange={handleChange}></textarea>
                 
                 <label htmlFor="firstname">Nombre</label>
-                <input type="text" id="firstname" onChange={handleChange}/>
+                <input type="text" id="firstname" name="firstname" onChange={handleChange}/>
 
                 <label htmlFor="lastmame">Apellidos</label>
-                <input type="text" id="lastname" onChange={handleChange}/>
+                <input type="text" id="lastname" name="lastname" onChange={handleChange}/>
 
                 <label htmlFor="birthdate">Cumpleaños</label>
-                <input type="date" id="birthdate" onChange={handleChange}/>
+                <input type="date" id="birthdate" name="birthdate" onChange={handleChange}/>
 
                 <button type="submit">Registrarse</button>
             </form>
