bac27194ccc9a875946a0ab5afedf4fcc16c7f6f
Alexander <adragar997@fp.iesromerovargas.com>
2025-05-05 20:54:34 +0200

    Install django pagination

--- config/settings.py
+++ config/settings.py
@@ -137,5 +137,7 @@ CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
