86dffb738855072c68a95029a4b600d006c666d7
Alexander <adragar997@fp.iesromerovargas.com>
2025-05-31 21:22:34 +0200

    try to deploy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-khd@m1xs(92q)1etkp9$v$uv%&8kewkkb7nbvnjea5*53m!7@='
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('trophyhunters/', include('trophy_hunters.urls')),
+    path('', include('trophy_hunters.urls')),
 ]
